c6b361740eefc3a1a35affee1d2f4c5c71be99f0
LeonidVVeriga <leonid.veriga@gmail.com>
2015-03-27 23:48:51 +0300

    [UPDATE] Added ignored deploy.bat and project dependency for TestSite on ClientTestLibrary.

---
+++ ClientTestLibrary/Bridge/build/deploy.bat
@@ -0,0 +1,8 @@
+﻿rem This is to clear all js files in the target directory
+rem Then to copy all js files from source to target directory
+rem copy /y NUL EmptyFile.txt >NUL
+rem Please note there is a pre-build command line to clear source directory before script generation
+
+cd "..\..\..\TestSite\Tests"
+del *.js /f /s /q
+xcopy "..\..\ClientTestLibrary\Bridge\output\*.js" "." /Y /I /S
