9dafde9a21643c3fe1337d5c97147661f89624ac
Mimisss <dimitris@object.net>
2015-05-02 18:52:08 +0300

    Added tests for String methods in TestStringFunctions.cs

--- tests/client/Tests/SystemFunctions/TestStringFunctions.cs
+++ tests/client/Tests/SystemFunctions/TestStringFunctions.cs
@@ -11,15 +11,66 @@ namespace ClientTestLibrary
         // String functions
         public static void Strings(Assert assert)
         {
-            assert.Expect(2);
+            assert.Expect(21);
 
-            // TEST
-            var msg = "HELLO".ToLowerCase();
-            assert.DeepEqual(msg, "hello", "'HELLO'.ToLowerCase()");
+            // TEST ToLowerCase
+            var s = "HELLO".ToLowerCase();
+            assert.DeepEqual(s, "hello", "'HELLO'.ToLowerCase()");
 
-            // TEST
-            msg = "hello".ToUpperCase();
-            assert.DeepEqual(msg, "HELLO", "'hello'.ToUpperCase()");
+            // TEST ToUpperCase
+            s = "hello".ToUpperCase();
+            assert.DeepEqual(s, "HELLO", "'hello'.ToUpperCase()");
+
+            s = "Hello Bridge.NET";
+            // TEST String(string) constructor
+            assert.DeepEqual(new String(s), s, "new String('" + s + "')");
+
+            // TEST String(char, count) constructor
+            assert.DeepEqual(new String('-', 4), "----", "new String('-',4)");
+
+            // TEST IndexOfAny
+            char[] anyOf = new char[] { 'x', 'b', 'i' };
+            string sAnyOf = "['x','b','i']";
+
+            assert.DeepEqual(s.IndexOfAny(anyOf), 8, "'" + s + "'.IndexOfAny(" + sAnyOf + ")");
+            assert.Throws(() => s.IndexOfAny(anyOf, 18, 8), "'" + s + "'.IndexOfAny(" + sAnyOf + ")");
+            assert.Throws(() => s.IndexOfAny(null), "'" + s + "'.IndexOfAny(null)");
+
+            s = string.Empty;
+            assert.DeepEqual(s.IndexOfAny(anyOf), -1, "String.Empty.IndexOfAny(" + sAnyOf + ")");
+            
+            s = null;
+            assert.DeepEqual(s.IndexOfAny(anyOf), -1, "null.IndexOfAny(" + sAnyOf + ")");
+
+            // TEST IndexOf
+            s = "Hello Bridge.NET";
+
+            assert.DeepEqual(s.IndexOf('e'), 1, "'" + s + "'.IndexOf('e')");
+            assert.DeepEqual(s.IndexOf("e."), 11, "'" + s + "'.IndexOf('e.')");
+            assert.DeepEqual(s.IndexOf('e', 6, 8), 11, "'" + s + "'.IndexOf('e', 6, 8)");            
+            assert.Throws(() => s.IndexOf(null), "'" + s + "'.IndexOf(null)");
+
+            s = string.Empty;
+            assert.DeepEqual(s.IndexOf('e'), -1, "String.Empty.IndexOf('e')");
+
+            s = null;
+            assert.DeepEqual(s.IndexOf('e'), -1, "null.IndexOf('e')");
+
+            // TEST Compare
+            string s1 = "Animal";
+            string s2 = "animal";
+
+            assert.DeepEqual(string.Compare(s1, s2, true), 0, "String.Compare('" + s1 + "', '" + s2 + "', true)");
+            assert.DeepEqual(string.Compare(s1, s2, false), 1, "String.Compare('" + s1 + "', '" + s2 + "', false)");
+
+                        
+            // TEST Contains            
+            s = "Hello Bridge.NET";
+
+            assert.DeepEqual(s.Contains("Bridge"), true, "'" + s + "'.Contains('Bridge')");
+            assert.DeepEqual(s.Contains(String.Empty), true, "'" + s + "'.Contains(String.Empty)");
+            assert.DeepEqual(String.Empty.Contains("Bridge"), false, "String.Empty.Contains('Bridge')");
+            assert.Throws(() => s.Contains(null), "null.Contains('Bridge')");
         }
     }
 }
