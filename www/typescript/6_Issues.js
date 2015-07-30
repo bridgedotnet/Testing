d7bb9ccf3f76a987302923ca3af596ed25b0ce1a
LeonidVeriga <leonid@object.com>
2015-07-30 22:39:45 +0300

    [TEST] [bridgedotnet/Bridge#336] TypeScript part

---
+++ www/typescript/6_Issues.js
@@ -0,0 +1,11 @@
+﻿/// <reference path="..\..\www\qunit\qunit.d.ts" />
+/// <reference path="..\..\www\typescript\bridge.d.ts" />
+QUnit.module("TypeScript - Issues");
+
+QUnit.test("#336", function (assert) {
+    var l1 = new (Bridge.List$1(String))(["4"]);
+    var l2 = new (Bridge.List$1(String))(["1", "2"]);
+
+    l1.insertRange(0, l2);
+    assert.deepEqual(l1.toArray(), ["1", "2", "4"], "InsertRange works (1)");
+});
