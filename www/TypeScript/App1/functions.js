aa1b3fb03f57de36962c9a99fd619179ce95f155
LeonidVeriga <leonid@object.net>
2016-04-06 15:09:47 +0300

    Restore TypeScript folder trying to fix auto-deploy

---
+++ www/TypeScript/App1/functions.js
@@ -0,0 +1,33 @@
+﻿(function (globals) {
+    "use strict";
+
+    Bridge.define('Functions.DelegateClass', {
+        methodVoidDelegate: null,
+        methodStringDelegate: null,
+        methodStringDelegateIntResult: null
+    });
+    
+    Bridge.define('Functions.DelegateInterface');
+    
+    Bridge.define('Functions.Delegates');
+    
+    Bridge.define('Functions.Parameters', {
+        getSomething: function (i) {
+            if (i === void 0) { i = 5; }
+            return i;
+        },
+        join: function (numbers) {
+            if (numbers === void 0) { numbers = []; }
+            var s = "";
+            for (var i = 0; i < numbers.length; i = (i + 1) | 0) {
+                s = s + numbers[i];
+            }
+    
+            return s;
+        }
+    });
+    
+    
+    
+    Bridge.init();
+})(this);
